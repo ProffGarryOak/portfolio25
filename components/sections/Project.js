28a05149be51ba3e7d54d37ac9260c11e917f27c
proffgarryoak <proffgarryoak2@gmail.com>
2025-04-02 22:24:33 +0530

    first commit

--- components/sections/Project.js
+++ components/sections/Project.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import ProjectCard from '@/Components/ui/ProjectCard';
+import ProjectCard from '@/Components/UI/ProjectCard';
 
 const projects = [
   {
